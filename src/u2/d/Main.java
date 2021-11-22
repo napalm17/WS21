f411ac996c4d30028290b7ba4fab5f61fc18ad06
napalm17 <suerberkay17@gmail.com>
2021-11-22 15:20:47 +0100

    change

--- src/u2/d/Main.java
+++ src/u2/d/Main.java
@@ -2,7 +2,7 @@ package u2.d;
 
 public class Main {
     public static void main(String[] args) {
-        var result = PaswordTools.isPasswordLeaked("123456abcdHAUS", "");
+        boolean result = PaswordTools.isPasswordLeaked("123456abcdHAUS", "");
         System.out.println(result);
     }
 }
