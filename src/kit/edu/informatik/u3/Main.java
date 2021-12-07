03d733ce437061c0a956c30f80dd861a217db5f0
napalm17 <suerberkay17@gmail.com>
2021-12-07 23:24:41 +0100

    changes

--- src/kit/edu/informatik/u3/Main.java
+++ src/kit/edu/informatik/u3/Main.java
@@ -2,14 +2,14 @@ package kit.edu.informatik.u3;
 
 public class Main {
     public static void main(String[] args) {
-        UnrolledLinkedList list1 = new UnrolledLinkedList(3);
-        for (int i = 0; i < 10; i++) {
+        UnrolledLinkedList list1 = new UnrolledLinkedList(4);
+        for (int i = 6; i < 84; i++) {
             list1.push(i);
         }
         // list1.pop();
-        // System.out.println(list1.get(4));
-        // System.out.println(list1.size());
         System.out.println(list1.toString("-"));
+        System.out.println(list1.get(2));
+        System.out.println(list1.size());
 
     }
 }
--- src/kit/edu/informatik/u3/UnrolledLinkedList.java
+++ src/kit/edu/informatik/u3/UnrolledLinkedList.java
@@ -1,21 +1,24 @@
 package kit.edu.informatik.u3;
 
+/**
+ * Class for Task B
+ * @author ugpsy
+ * @version 1.0
+ */
 public class UnrolledLinkedList {
-    private int arraySize;
+    private final int arraySize;
 
     private class ListCell {
         int[] array;
         ListCell prev;
         ListCell next;
-
         ListCell(int[] a, ListCell p, ListCell n) {
             this.array = a;
             this.prev = p;
             this.next = n;
         }
-
-
     }
+
     private ListCell first, last;
     public UnrolledLinkedList(int arraySize) {
         this.arraySize = arraySize;
@@ -24,54 +27,40 @@ public class UnrolledLinkedList {
 
     public boolean push(int element) {
         ListCell newListCell = new ListCell(new int[] {element}, this.last, null);
-
         if (this.first == null) { // add to empty list.
             this.first = this.last = newListCell;
-
+            // return true;
         } else if (this.last.array.length == this.arraySize) { // create new array if the last array is full.
             this.last.next = newListCell;
             this.last = newListCell;
-
-
+            // return true;
         } else { // add to the last list whose size is under the given maximum array size.
-            int[] temp = this.last.array;
-            this.last.array = new int[temp.length + 1];
-            System.arraycopy(temp, 0, this.last.array, 0, temp.length);
-            this.last.array[temp.length] = element;
+            this.last.array = extendArray(this.last.array, element);
+            // return false;
         }
-
-        printList(this.last.array);
         return true;
     }
 
     public boolean pop() {
-        System.out.println();
         if (this.last.array.length == 1) {
             this.last = this.last.prev;
             return true;
         }
         else {
-            int[] temp = this.last.array;
-            this.last.array = new int[temp.length - 1];
-            System.arraycopy(temp, 0, this.last.array, 0, temp.length - 1);
+            this.last.array = shrinkArray(this.last.array);
             return false;
         }
-        /*
-        printList(this.last.prev.prev.array);
-        printList(this.last.prev.array);
-        printList(this.last.array);
-        return true; */
     }
 
     public int get(int i) {
+        ListCell cell = this.first;
         int index = 0;
-        var cell = this.first;
         while (cell != null) {
-            for (int elem:
-                 cell.array) {
-
-                if (elem == i) { return index; }
-                index += 1;
+            for (int element : cell.array) {
+                if (index == i) {
+                    return element;
+                }
+                index++;
             }
             cell = cell.next;
         }
@@ -79,35 +68,51 @@ public class UnrolledLinkedList {
     }
 
     public int size() {
-        var cell = this.first;
+        ListCell cell = this.first;
         int index = 0;
-        while (cell.next != null) {
-            index += arraySize;
+        while (cell != null) {
+            index += cell.array.length;
             cell = cell.next;
         }
-        index += this.last.array.length;
         return index;
     }
 
     public String toString(String separator) {
         String result = "";
-        var cell = this.first;
+        ListCell cell = this.first;
         while (cell != null) {
-            for (int elem:
+            for (int element:
                     cell.array) {
-                result += elem + separator;
+                result += element + separator;
             }
             cell = cell.next;
         }
         return result.substring(0, result.length() - 1);
     }
+
+    private int[] extendArray(int[] oldArray, int newElem) {
+        int[] extendedArray = new int[oldArray.length + 1];
+        for (int i = 0; i < oldArray.length; i++) {
+            extendedArray[i] = oldArray[i];
+        }
+        extendedArray[oldArray.length] = newElem;
+        return extendedArray;
+    }
+
+    private int[] shrinkArray(int[] oldArray) {
+        int[] extendedArray = new int[oldArray.length - 1];
+        for (int i = 0; i < oldArray.length - 1; i++) {
+            extendedArray[i] = oldArray[i];
+        }
+        return extendedArray;
+    }
+
     private void printList(int[] array) {
         for (int elem:
-             array) {
+                array) {
             System.out.print(elem);
             System.out.print(" ");
         }
         System.out.println();
     }
-
 }
